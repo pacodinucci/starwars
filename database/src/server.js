a993ef39da69769731196b55898dd2c1225a4c5e
Francisco Di Nucci <franciscoldinucci@gmail.com>
2023-08-22 11:25:14 -0300

    cors

--- database/src/server.js
+++ database/src/server.js
@@ -9,6 +9,13 @@ server.use(express.json());
 
 server.use(require("./routes"));
 
+server.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
 server.use('*', (req, res) => {
     res.status(404).send('Not Found');
 });

303f587d764f67b3ae624b9bd492563b64a24675
pacodinucci <franciscoldinucci@gmail.com>
2023-08-29 15:27:19 -0300

    cors database

--- database/src/server.js
+++ database/src/server.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const cors = require('cors');
 
 
 const server = express();
 
-
-
 server.use(morgan('dev'));
 server.use(express.json());
 
+server.use(cors());
+
 server.use(require("./routes"));
 
-server.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
+// server.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//     next();
+//   });
 
 server.use('*', (req, res) => {
     res.status(404).send('Not Found');

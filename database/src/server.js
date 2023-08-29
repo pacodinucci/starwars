7d6709234c43c36fc89c086c366bf784aa5a8fcb
pacodinucci <franciscoldinucci@gmail.com>
2023-08-29 12:21:58 -0300

    cors bla bla

--- database/src/server.js
+++ database/src/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
+
 
 
 const server = express();
 
-server.use(cors());
+
 
 server.use(morgan('dev'));
 server.use(express.json());
@@ -13,7 +13,7 @@ server.use(express.json());
 server.use(require("./routes"));
 
 server.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
